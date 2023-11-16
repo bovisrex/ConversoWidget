1c6d1558c35fb74ccd1b480b256a9afdc0b33fd2
bovisrex <bovis.rex@gmail.com>
2023-11-16 18:38:15 -0500

    Add files via upload

---
+++ Convo Console App/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Title = "Conversion App Test";
+
+decimal convoFrom = 0.00m;
+decimal convoResult = 0.00m;
+decimal value = 0.00m;
+
+
+Console.Write("Enter degrees in Celsius: ");
+convoFrom = Convert.ToDecimal(Console.ReadLine());
+value = celsiusToFahrenheit();
+Console.WriteLine($"{value} degrees Fahrenheit.");
+
+decimal celsiusToFahrenheit()
+{
+    convoResult = ((convoFrom * 9) / 5) + 32;
+    return convoResult;
+}
+decimal fahrenheitToCelsius()
+{
+    convoResult = ((convoFrom - 32) * 5 / 9);
+    return convoResult;
+}
